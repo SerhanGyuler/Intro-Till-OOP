1f604be14bd9097648606cf5fbeb88a27e83830c
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-10-27 12:02:45 +0100

    Copy and pase the code from Cricle.cs since it did not upload to github

--- Intro Till OOP/Program.cs
+++ Intro Till OOP/Program.cs
@@ -7,5 +7,21 @@
             Console.WriteLine("Hello, World!");
         }
 
+        public class Circle
+        {
+
+            private double radius;
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public double GetArea()
+            {
+                return radius * radius * Math.PI;
+            }
+
+        }
     }
 }
